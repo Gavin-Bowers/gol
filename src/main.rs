beab9393dd9b4bcfa62d50cda9997cc5da50ee4b
Gavin Bowers <gbowersformal@gmail.com>
2022-12-11 09:40:33 -0800

    fixed rendering and added fps reading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::time::Instant;
 use std::vec;
 
-use ggez::event::EventHandler;
 use rand::Rng;
 
 use ggez::conf;
@@ -10,20 +10,42 @@ use ggez::event;
 use ggez::timer;
 use ggez::graphics::{self, Color};
 //use ggez::input::keyboard::KeyCode;
+use ggez::event::EventHandler;
 use ggez::{Context, ContextBuilder, GameResult};
 
-const TILES_SPANNING: u32 = 500; //The number of tiles spanning the board
-const TILE_SIZE: u32 = 2; //Width of one tile in Pixels
-const SCREEN_SIZE: f32 = 
-    (TILES_SPANNING * TILE_SIZE) as f32;
+const DESIRED_FPS: u32 = 300;
+const TILES_SPANNING: u32 = 100; //The number of tiles spanning the board
+const AUTO_SCALE_TILES: bool = true;
+const TILE_SIZE: u32 = if AUTO_SCALE_TILES {1000 / TILES_SPANNING} else {10} ; //Width of one tile in Pixels
+const SCREEN_SIZE: f32 = (TILES_SPANNING * TILE_SIZE) as f32;
 
-//type Cell = (i32, i32);
+type Cell = (i32, i32);
 
+fn cell_to_rect(cell: &Cell) -> graphics::Rect {
+    graphics::Rect::new_i32(
+        TILE_SIZE as i32 * cell.0,
+        TILE_SIZE as i32 * cell.1,
+        TILE_SIZE as i32,
+        TILE_SIZE as i32,
+    )
+}
+
+fn draw_cell(cell: &Cell, canvas: &mut graphics::Canvas)  {
+    canvas.draw(
+        &graphics::Quad,
+        graphics::DrawParam::new()
+            .dest_rect(cell_to_rect(cell))
+            .color([1.,1.,1.,1.]),
+        );
+}
+
+/*
 #[derive(PartialEq, Eq, Hash)]
 struct Cell {
     x: i32,
     y: i32
 }
+
 
 impl Cell {
     pub fn new(x: &i32, y: &i32) -> Self {
@@ -54,28 +76,52 @@ impl From<&Cell> for graphics::Rect { //Helper function to turn cell into a rect
         )
     }
 }
+*/
 
 //################################################
 //Supporting functions which assist generation
 //################################################
 
 fn neighbours(cell: &Cell) -> Vec<Cell> {
-    let x: i32 = cell.x;
-    let y: i32 = cell.y;
+    let x: i32 = cell.0;
+    let y: i32 = cell.1;
     vec![
-    Cell::new(&(x-1),&(y-1)), Cell::new(&x,&(y-1)), Cell::new(&(x+1),&(y-1)),
-    Cell::new(&(x-1),&y),                           Cell::new(&(x+1),&y),
-    Cell::new(&(x-1),&(y+1)), Cell::new(&x,&(y+1)), Cell::new(&(x+1),&(y+1)),
+    (x-1,y-1), (x,y-1), (x+1,y-1),
+    (x-1,y),            (x+1,y),
+    (x-1,y+1), (x,y+1), (x+1,y+1),
     ]
 }
 
+/*
+    Cell::new(&(x-1),&(y-1)), Cell::new(&x,&(y-1)), Cell::new(&(x+1),&(y-1)),
+    Cell::new(&(x-1),&y),                           Cell::new(&(x+1),&y),
+    Cell::new(&(x-1),&(y+1)), Cell::new(&x,&(y+1)), Cell::new(&(x+1),&(y+1)),
+*/
+
 fn neighbour_counts(col: &HashSet<Cell>) -> HashMap<Cell, i32> {
     let mut ncnts = HashMap::new();
-    for cell in col.iter().flat_map(neighbours) {
-        *ncnts.entry(cell).or_insert(0) += 1;
+    for neighbor in col.iter().flat_map(neighbours) {
+        *ncnts.entry(neighbor).or_insert(0) += 1;
     }
     ncnts
 }
+
+/*
+So the way the above code works (took me too long to figure out) is that cel.iter() makes an iterator
+for the colony (all cells) which has flat_map called on it, with the function neighbors given 
+as a parameter. flat_map applies the neighbors function to each cell, producing a Vec of new neighbor cells
+All of the elements in these vecs are added to a single iterator by flat_map (that's how it works),
+which is what "cell in" iterates through. So each cell which neighbors a live cell is visited, 
+and there is redundancy. Cells which have multiple living neighbors appear more than once. once for each
+neighbor in fact. And so, the .entry command adds each cell which is adjacent to a living one 
+(hereby shortened to neighbor), or not, if it is already present, to the new hashmap.
+.entry returns a mutable reference to the value tied to the key, which is then initialized to 0
+by .or_insert(0) if needed, and then 1 is added to the value. A dereference ("*"") is needed because .or_insert
+returns a mutable reference. 1 is added each time we "visit" a cell, so the result is a map of each cell
+(alive or dead) with 1 or more living neighbors, and the number of living neighbors it has. You may be thinking,
+"what about the cell in the middle? where is it's count?" And the answer is that if a cell has 0 neighbors,
+it's gonna die so it doesn't matter lol
+*/
 
 fn generation(col: &HashSet<Cell>) -> HashSet<Cell> {
     neighbour_counts(&col)
@@ -97,7 +143,7 @@ fn random_cells() -> HashSet<Cell> {
         for column in 0..TILES_SPANNING
         {
             if rng.gen_range(0..2) == 1 {
-                cells.push(Cell::new(&(row as i32),&(column as i32)));
+                cells.push(((row as i32),(column as i32)));
             }
         }
     }
@@ -108,6 +154,8 @@ fn random_cells() -> HashSet<Cell> {
 struct MainState { //Used to contain all the information of the game while running
     screen: graphics::ScreenImage,
     col: HashSet<Cell>,
+    frame_count: i32,
+    start_time: std::time::Instant,
 }
 
 impl MainState {
@@ -119,6 +167,8 @@ impl MainState {
         let state = MainState {
             screen,
             col,
+            frame_count: 0,
+            start_time: Instant::now(),
         };
         Ok(state)
     }
@@ -126,27 +176,46 @@ impl MainState {
 
 impl EventHandler for MainState {
     fn update(&mut self, ctx: &mut Context) -> GameResult {
-        const DESIRED_FPS: u32 = 30; //Using framerates higher than 30 cause terrible lag for some reason
-
+        
         while ctx.time.check_update_time(DESIRED_FPS) {
             //let seconds = 1.0 / (DESIRED_FPS as f32);
             let new_col = generation(&self.col);
             self.col = new_col;
 
+            //used for framerate calculation
+            self.frame_count += 1;
+            break; //Very important for running below intended fps and not included in demo...
         }
+        
+        //Show actual framerate
+        if self.frame_count >= 30 {
+            let elapsed_time = self.start_time.elapsed();
+            let fps = self.frame_count as f64 / elapsed_time.as_secs_f64();
+            println!("fps: {:.0}", fps);
+            self.start_time = Instant::now();
+            self.frame_count = 0;
+        }
+        
+
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_screen_image(ctx, &mut self.screen, Color::BLACK);
 
-        for cell in self.col.iter() {
-            cell.draw(&mut canvas);
-        }
+        //let time1 = self.start_time.elapsed();
 
+        for cell in self.col.iter() {
+            draw_cell(cell, &mut canvas);
+        }
+        /*
+        let time2 = self.start_time.elapsed();
+        let draw_time = time2 - time1;
+        println!("It takes {} seconds to draw", draw_time.as_secs_f64());
+        */
         canvas.finish(ctx)?;
         ctx.gfx.present(&self.screen.image(ctx))?;
-
+        
         timer::yield_now();
         Ok(())
     }
@@ -169,7 +238,3 @@ fn main() -> GameResult {
 
     event::run(ctx, events_loop, game_state)
 }
-
-//life(glider, 20, 8, 8);
-
-//life(random_cells(15,15), 20, 15, 15);
